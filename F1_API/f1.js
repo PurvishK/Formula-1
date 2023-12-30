b91fda51e5055ce548292f407aaa0109bb5d42fc
Purvish Kodi <purvish.kodi@wabtec.com>
2023-12-30 20:16:53 +0530

    setup

--- F1_API/f1.js
+++ F1_API/f1.js
@@ -1,22 +1,45 @@
-const express = require('express');
-const axios = require('axios');
-const f1Controller = new require('f1_controller') ();
-const fs = require('fs');
-const XMLtoJSON = require('xml2js');
-const parser = new XMLtoJSON.Parser();
-const app = express();
+/** @format */
+
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const axios = require("axios");
+///////////////////////////////
+const f1ControllerFile = require("./Controllers/f1_controller");
+const f1Controller = new f1ControllerFile();
+//////////////////////////////
 const PORT = 8842;
-
-app.get('/getAllDrivers',async (req,res)=>{
-    console.log("In get All Drivers");
-    let allDriverXML = await axios.get('http://ergast.com/api/f1/2023/drivers');
-    const allDriversJson = f1Controller.xmlJson(allDriverXML.data.trim())
-
-    console.log(allDriversJson);
-    res.send(allDriversJson);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+	}
 });
 
-app.listen(PORT, ()=>{
-    console.log("Running server in ", PORT);
+//////////////////////////////////////
+
+
+// app.get("/",(req,res)=>{
+//     res.status(200).sendFile(path.join(__dirname, '/index.html'));
+// })
+
+///////////////////////////////////
+app.get("/getAllDrivers", async (req, res) => {
+	console.log("In get All Drivers");
+	let allDriverXML = await axios.get("http://ergast.com/api/f1/2023/drivers");
+	const allDriversJson = f1Controller.xmlJson(allDriverXML.data.trim());
+
+	console.log(allDriversJson);
+	res.send(allDriversJson);
 });
 
+io.on("connection", (socket) => {
+	console.log("User ", socket, " logged in..");
+});
+
+///////////////////////////////////
+server.listen(PORT, () => {
+	console.log("Running server in ", PORT);
+});
--- F1_UI/src/app/app.component.ts
+++ F1_UI/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {io} from 'socket.io-client';
+import {EndpointsService} from "../services/endpoints.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'f1-ui';
+
+  constructor(private endPoints: EndpointsService) {
+  }
+
+  ngOnInit() {
+    const socket = io();
+    this.endPoints.getAllDriver().subscribe((res)=>{
+
+    });
+  }
 }
